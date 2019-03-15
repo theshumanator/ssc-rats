61a0624d045c7fd92e05d04c2e5304d98333af12
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-15 12:54:41 +0000

    link up heroku with the secret key

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     #port = os.getenv('PORT', default=8000)
     #updater.start_webhook(port=port)
-    app.run(port=port);
-    #app.run(host='0.0.0.0', port=port)
+    #app.run(port=port);
+    app.run(host='0.0.0.0', port=port)
