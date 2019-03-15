5c94bc96955dc22031361368b6e7123f80b1b8df
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-15 13:13:13 +0000

    secret is not being accessed

--- rats/AmazonS3/connectWithS3.py
+++ rats/AmazonS3/connectWithS3.py
@@ -1,9 +1,17 @@
 import boto3
+import os
+#from boto3.s3.connection import S3Connection
+
+
 
 
 def connectWithS3():
+    print (os.environ['AWS_SECRET_ACCESS_KEY'])
     # Create an S3 client
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    #s3 = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
+
 
     # Call S3 to list current buckets
     response = s3.list_buckets()
