035c7d88514ffca766fe05b8824bf2815052fb7a
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-15 13:22:24 +0000

    remove k

--- rats/AmazonS3/connectWithS3.py
+++ rats/AmazonS3/connectWithS3.py
@@ -8,10 +8,11 @@ import os
 def connectWithS3():
     print (os.environ['AWS_SECRET_ACCESS_KEY'])
     # Create an S3 client
-    s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    #s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    #aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
     #s3 = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
 
+    s3 = boto3.client('s3')
 
     # Call S3 to list current buckets
     response = s3.list_buckets()
