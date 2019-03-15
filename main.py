581be7bdf5f2ecf438f890abe2248210f4f701f1
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-15 11:23:24 +0000

    add port

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='testflask/templates')
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("/home.html")
 @app.route("/salvador")
 def salvador():
     return "Hello, Salvador"
@@ -10,4 +13,8 @@ def salvador():
 def about():
     return render_template("about.html")
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    #port = os.getenv('PORT', default=8000)
+    #updater.start_webhook(port=port)
+    app.run(host='0.0.0.0', port=port)
